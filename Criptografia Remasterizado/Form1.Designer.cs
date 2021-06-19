aa25a9aa286224bba595392861fd2c43632e5790
Gabriel Silva Araujo <86019173+MystGabriel@users.noreply.github.com>
2021-06-19 13:06:44 -0300

    Add files via upload

---
+++ Criptografia Remasterizado/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Criptografia_Remasterizado
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Encriptar = new System.Windows.Forms.Button();
+            this.Decriptar = new System.Windows.Forms.Button();
+            this.Limpar = new System.Windows.Forms.Button();
+            this.Código = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(715, 39);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(715, 39);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(39, 272);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(715, 39);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Encriptar
+            // 
+            this.Encriptar.Location = new System.Drawing.Point(39, 345);
+            this.Encriptar.Name = "Encriptar";
+            this.Encriptar.Size = new System.Drawing.Size(150, 46);
+            this.Encriptar.TabIndex = 3;
+            this.Encriptar.Text = "Encriptar";
+            this.Encriptar.UseVisualStyleBackColor = true;
+            this.Encriptar.Click += new System.EventHandler(this.Encriptar_Click);
+            // 
+            // Decriptar
+            // 
+            this.Decriptar.Location = new System.Drawing.Point(317, 345);
+            this.Decriptar.Name = "Decriptar";
+            this.Decriptar.Size = new System.Drawing.Size(150, 46);
+            this.Decriptar.TabIndex = 4;
+            this.Decriptar.Text = "Decriptar";
+            this.Decriptar.UseVisualStyleBackColor = true;
+            this.Decriptar.Click += new System.EventHandler(this.Decriptar_Click);
+            // 
+            // Limpar
+            // 
+            this.Limpar.Location = new System.Drawing.Point(604, 345);
+            this.Limpar.Name = "Limpar";
+            this.Limpar.Size = new System.Drawing.Size(150, 46);
+            this.Limpar.TabIndex = 5;
+            this.Limpar.Text = "Limpar";
+            this.Limpar.UseVisualStyleBackColor = true;
+            this.Limpar.Click += new System.EventHandler(this.Limpar_Click);
+            // 
+            // Código
+            // 
+            this.Código.AutoSize = true;
+            this.Código.Location = new System.Drawing.Point(337, 15);
+            this.Código.Name = "Código";
+            this.Código.Size = new System.Drawing.Size(203, 32);
+            this.Código.TabIndex = 7;
+            this.Código.Text = "Código de Senha:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 32);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Senha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 32);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Senha Encriptada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 32);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Senha Decriptada";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(562, 15);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(192, 39);
+            this.numericUpDown1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Código);
+            this.Controls.Add(this.Limpar);
+            this.Controls.Add(this.Decriptar);
+            this.Controls.Add(this.Encriptar);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Encriptar;
+        private System.Windows.Forms.Button Decriptar;
+        private System.Windows.Forms.Button Limpar;
+        private System.Windows.Forms.Label Código;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Criptografia Remasterizado/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Criptografia_Remasterizado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Limpar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+        }
+
+        private void Encriptar_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = string.Empty;
+            
+            for(int i = 0; i < textBox1.Text.Length; i++)
+            {
+                int txtUsuario = (int)textBox1.Text[i];
+                int txtCifrado = txtUsuario + Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                textBox2.Text += Char.ConvertFromUtf32(txtCifrado);
+            }
+        }
+
+        private void Decriptar_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = string.Empty;
+
+            for(int i = 0; i < textBox2.Text.Length; i++)
+            {
+                int txtCifrado = (int)textBox2.Text[i];
+                int txtOriginal = txtCifrado - Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                textBox3.Text += Char.ConvertFromUtf32(txtOriginal);
+            }
+        }
+    }
+}
